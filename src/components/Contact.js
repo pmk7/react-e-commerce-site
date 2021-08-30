7b17fdee861d0ffc53526ed3b8b68294947cf7d5
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-08-30 21:40:27 +0200

    added UI, fetched data from api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+   <div className="section-center">
+     <h3>Join our newsletter and get 20% off!</h3>
+     <div className="content">
+       <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ipsam magnam iure deserunt tempora, sed magni facere illo! Sed fugiat deleniti nesciunt aspernatur at quisquam nobis architecto ipsa, repudiandae pariatur!
+       </p>
+       <form className="contact-form">
+         <input type="email" className='form-input' placeholder='enter email'/>
+         <button type='submit' className='submit-btn'>
+           subscribe
+         </button>
+       </form>
+     </div>
+   </div>
+
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
