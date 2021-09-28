a09cf5cff5ba0c7a8096b1591b44a363b27fa104
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-09-28 18:06:55 +0200

    added sort functionality

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -3,7 +3,26 @@ import styled from 'styled-components';
 import { formatPrice } from '../utils/helpers';
 import { Link } from 'react-router-dom';
 const ListView = ({ products }) => {
-  return <Wrapper>list view</Wrapper>;
+  return (
+    <Wrapper>
+      {products.map((product) => {
+        const { id, image, name, price, description } = product;
+        return (
+          <article key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <h5 className="price">{formatPrice(price)}</h5>
+              <p>{description.substring(0, 150)}...</p>
+              <Link to={`/products/${id}`} className="btn">
+                Details
+              </Link>
+            </div>
+          </article>
+        );
+      })}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
