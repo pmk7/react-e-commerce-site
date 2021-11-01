3ce4398076ceb4159aaddb484bfb6d5f9952939b
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-11-01 22:09:56 +0100

    authentication feature

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev-wo9wqcg5.us.auth0.com
+// OGQSMRTnfkQCxokREC9px0usvIiF4Peg
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-wo9wqcg5.us.auth0.com"
+    clientId="OGQSMRTnfkQCxokREC9px0usvIiF4Peg"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
